944d3e3bc1f396c254e7e4068f86bff86242498d
liutaoMr <17806277535@163.com>
2019-08-17 15:54:39 +0800

    体检公卫commit8.17

---
+++ app/src/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import login from './views/login/login'
+import demo from './views/text/demo'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path:'/demo',
+      name:'demo',
+      component:demo
+    }
+  ]
+})
---
+++ app/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout class="home">
+    <headNav></headNav>
+    <Content>
+      <information-collection></information-collection>
+      <barcode-handle></barcode-handle>
+    </Content>
+
+  </Layout>
+</template>
+<script>
+  import headNav from './nav/headNav'
+  import InformationCollection from './informationCollection/InformationCollection'
+  import BarcodeHandle from './barcodeHandle/barcodeHandle'
+
+  export default {
+    mounted(){
+      // document.onkeydown = function(e) {
+      //   var keyCode = e.keyCode || e.which || e.charCode;
+      //   var ctrlKey = e.ctrlKey || e.metaKey;
+      //   if(ctrlKey && keyCode == 123) {
+      //     alert('ctrl+s')
+      //   }
+      //   e.preventDefault();
+      //   return false;
+      // }
+    },
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      BarcodeHandle,
+      headNav,
+      InformationCollection,
+    },
+    computed: {
+      rotateIcon () {
+        return [
+          'menu-icon',
+          this.isCollapsed ? 'rotate-icon' : ''
+        ];
+      },
+      menuitemClasses () {
+        return [
+          'menu-item',
+          this.isCollapsed ? 'collapsed-menu' : ''
+        ]
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
