5cb1ac8ede163ba0a6823ec4a1b221c8ea0fe0db
liutaoMr <17806277535@163.com>
2019-08-17 16:36:57 +0800

    体检操作主页面

---
+++ app/src/views/handle/handle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="handleBox">
+    <head-nav></head-nav>
+    <div class="simpleInfor">
+      <span>条码号：</span><span>1234567890000</span>
+    </div>
+  </div>
+</template>
+
+<script>
+    import HeadNav from "../nav/headNav";
+    export default {
+        name: "handle",
+      components: {HeadNav}
+    }
+</script>
+
+<style scoped>
+.simpleInfor{
+  width: 100%;
+  background: #F4F3FC;
+}
+</style>
